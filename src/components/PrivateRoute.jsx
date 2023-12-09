96f4172039ab5d75edf0fe9d1f04dcdc868f88d6
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-09 22:46:01 +0530

    create public and private route

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Redirect, Route } from 'react-router-dom/cjs/react-router-dom.min';
+
+const PrivateRoute = ({ children, ...routeProps }) => {
+  const profile = false;
+
+  if (!profile) return <Redirect to="/signin" />;
+  return <Route {...routeProps}>{children}</Route>;
+};
+
+export default PrivateRoute;
