78d9016c42995154d6883e005c30e8a968189882
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-10 11:30:24 +0530

    add sign in page and sign in with google functionality

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,47 @@
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { auth, database } from '../misc/firebase';
+import firebase from 'firebase/app';
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success(`Welcome ${user.displayName}`, 4000);
+    } catch (error) {
+      Alert.error(error.message, 4000);
+    }
+  };
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>Progressive Chat Platform for Neophytes</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
