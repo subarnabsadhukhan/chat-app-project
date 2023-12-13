2aab3cf82e6e2361f4ac0496ea32d5bbd614bf74
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-13 09:10:15 +0530

    update nickname inside database

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,12 +1,22 @@
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import EditableInput from '../EditableInput';
+import { database } from '../../misc/firebase';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    console.log(newData);
+    const userNickNameRef = database
+      .ref(`profiles/${profile.id}`)
+      .child('name');
+
+    try {
+      await userNickNameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
   };
 
   return (
