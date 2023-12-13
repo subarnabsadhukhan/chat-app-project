4bc419d7dc45987f04f48de7bcce444bf3073188
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-13 06:37:03 +0530

    start building the sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className="h-100 pt-2">
+      <div>
+        <DashboardToggle />
+      </div>
+      buttom
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/dashboard/DashboardToggle.jsx
@@ -0,0 +1,19 @@
+import { Button, Drawer, Icon } from 'rsuite';
+import { useModalState } from '../../misc/custom-hooks';
+import Dashboard from '.';
+
+const DashboardToggle = () => {
+  const { isOpen, open, close } = useModalState();
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" /> Dashboard
+      </Button>
+      <Drawer show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  );
+};
+
+export default DashboardToggle;
