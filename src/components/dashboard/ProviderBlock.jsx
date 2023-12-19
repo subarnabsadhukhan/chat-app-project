15368873439f470266862420326e934006a74d28
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-19 09:12:14 +0530

    create provider block

---
+++ src/components/dashboard/ProviderBlock.jsx
@@ -0,0 +1,13 @@
+import { Icon, Tag } from 'rsuite';
+
+const ProviderBlock = () => {
+  return (
+    <div>
+      <Tag color="green">
+        <Icon icon="google" /> Connected
+      </Tag>
+    </div>
+  );
+};
+
+export default ProviderBlock;
