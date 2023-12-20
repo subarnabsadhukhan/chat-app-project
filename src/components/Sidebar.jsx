fac5a989a694f5e3d142ef785a3a613a0bbda76e
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-20 21:51:54 +0530

    start to work with chatroom list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,28 @@
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
+import { useEffect, useRef, useState } from 'react';
 
 const Sidebar = () => {
+  const topSidebarRef = useRef();
+
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal />
+        <Divider>Join Conversation</Divider>
       </div>
-      bottom
+      <ChatRoomList aboveElementHeight={height} />
     </div>
   );
 };
