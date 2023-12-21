4ed194d2ad0357d02688fde56784e4493960b616
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-21 08:55:22 +0530

    display chat rooms

--- src/components/rooms/ChatRoomList.jsx
+++ src/components/rooms/ChatRoomList.jsx
@@ -1,7 +1,11 @@
-import { Nav } from 'rsuite';
+import { Loader, Nav } from 'rsuite';
 import RoomItem from './RoomItem';
+import { useRooms } from '../../context/rooms.context';
+import { Link, useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ChatRoomList = ({ aboveElementHeight }) => {
+  const rooms = useRooms();
+  const location = useLocation();
   return (
     <Nav
       appearance="subtle"
@@ -9,10 +13,23 @@ const ChatRoomList = ({ aboveElementHeight }) => {
       reversed
       className="overflow-y-scroll custom-scroll"
       style={{ height: `calc(100% - ${aboveElementHeight}px)` }}
+      activeKey={location.pathname}
     >
-      <Nav.Item>
-        <RoomItem />
-      </Nav.Item>
+      {!rooms && (
+        <Loader center vertical content="Loading" speed="slow" size="md" />
+      )}
+      {rooms &&
+        rooms.length > 0 &&
+        rooms.map(room => (
+          <Nav.Item
+            componentClass={Link}
+            to={`/chat/${room.id}`}
+            key={room.id}
+            eventKey={`/chat/${room.id}`}
+          >
+            <RoomItem room={room} />
+          </Nav.Item>
+        ))}
     </Nav>
   );
 };
--- src/context/rooms.context.jsx
+++ src/context/rooms.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { database } from '../misc/firebase';
 import { transformToArrWithID } from '../misc/helper';
 
@@ -19,3 +19,5 @@ export const RoomsProvider = ({ children }) => {
     <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
   );
 };
+
+export const useRooms = () => useContext(RoomsContext);
