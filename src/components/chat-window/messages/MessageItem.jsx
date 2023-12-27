fee1c1cea26d606f767d13feb9c0f8ec0abce076
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-27 21:35:36 +0530

    handle message likes

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -5,11 +5,11 @@ import { Button } from 'rsuite';
 import { memo } from 'react';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { auth } from '../../../misc/firebase';
-import { useHover } from '../../../misc/custom-hooks';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({ message, handleAdmin }) => {
-  const { createdAt, text, author } = message;
+const MessageItem = ({ message, handleAdmin, handleLike }) => {
+  const { createdAt, text, author, likeCount, likes } = message;
 
   const admins = useCurrentRoom(v => v.admins);
   const isAdmin = useCurrentRoom(v => v.isAdmin);
@@ -17,10 +17,15 @@ const MessageItem = ({ message, handleAdmin }) => {
   const isMsgAuthorAdmin = admins.includes(author.id);
   const isAuthor = auth.currentUser.uid === author.id;
   const canGrantAdminPrivilege = isAdmin && !isAuthor;
-  const [selfRef, isHover] = useHover();
+  const [selfRef, isHovered] = useHover();
+
+  const isMobile = useMediaQuery('(max-width: 992px)');
+  const canShowIcons = isMobile || isHovered;
+  const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
+
   return (
     <li
-      className={`padded mb-1 cursor-pointer ${isHover ? 'bg-black-02' : ''}`}
+      className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`}
       ref={selfRef}
     >
       <div className="d-flex align-items-center font-bolder mb-1">
@@ -42,13 +47,12 @@ const MessageItem = ({ message, handleAdmin }) => {
           datetime={createdAt}
         />
         <IconBtnControl
-          // eslint-disable-next-line no-constant-condition
-          {...(true ? { color: 'red' } : {})}
-          isVisible
+          {...(isLiked ? { color: 'red' } : {})}
+          isVisible={canShowIcons}
           iconName="heart"
           tooltip="Like this message"
-          onClick={() => {}}
-          badgeContent={5}
+          onClick={() => handleLike(message.id)}
+          badgeContent={likeCount}
         />
       </div>
 
